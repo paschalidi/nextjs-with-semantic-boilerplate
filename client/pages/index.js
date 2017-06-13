0ed98307fd7e08642698f1f9314b133ac3bae3a3
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-13 09:56:30 +0200

    changes location of ocntantful-constants.js
     as it is only client realted file makes more sense to be inside the client file

--- client/pages/index.js
+++ client/pages/index.js
@@ -10,14 +10,11 @@ import React from 'react';
 import withRedux from 'next-redux-wrapper';
 import * as contentful from 'contentful'
 import initStore from '../store';
-
+import { ACCESS_TOKEN, SPACE_ID, AN_ENTRY } from '../contentful-constants';
 import Layout from '../components/Layout/index';
 
 class Index extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    const SPACE_ID = 't6jl7vzuhly7';
-    const ACCESS_TOKEN = '97fd086dac70531e4eb63ed203557cf2abcb17e01d294492543273427a40798d';
-    const AN_ENTRY = '77fQvB40s8Ugmsuu0aqMcI';
     let client = contentful.createClient({
       space: SPACE_ID,
       accessToken: ACCESS_TOKEN,
