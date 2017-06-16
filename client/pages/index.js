6639b26b56b29b9a2b94c47bc0e1344ea49a7719
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-16 09:50:59 +0200

    removes conentful example from index

--- client/pages/index.js
+++ client/pages/index.js
@@ -7,26 +7,12 @@
 
 import React from 'react';
 import withRedux from 'next-redux-wrapper';
-import * as contentful from 'contentful'
 import initStore from '../store';
-import { ACCESS_TOKEN, SPACE_ID, HEADER_ENTRY } from '../contentful';
+
 import Layout from '../components/Layout/index';
 
 class Index extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
-    let client = contentful.createClient({
-      space: SPACE_ID,
-      accessToken: ACCESS_TOKEN,
-    });
-
-    client.getEntries()
-      .then((response) => console.log(response.items))
-      .catch(console.error);
-
-    client.getEntry(HEADER_ENTRY)
-      .then((entry) => console.log(entry))
-      .catch(console.error)
-
     return (
       <Layout title="Vimcar">
         <div className="hello">
