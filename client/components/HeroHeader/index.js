35a6ea305d54ae4e37e065f896475053f4f067b6
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-21 17:41:31 +0200

    connects HeroHeader with contentful

--- client/components/HeroHeader/index.js
+++ client/components/HeroHeader/index.js
@@ -10,10 +10,10 @@
 import React from 'react';
 
 import * as contentful from 'contentful';
-import { SPACE_ID, ACCESS_TOKEN, IMAGE_ID } from './constants';
+import { SPACE_ID, ACCESS_TOKEN, ENTRY_ID, IMAGE_ID } from './constants';
 import Loading from '../../components/Loading';
 
-import {} from 'semantic-ui-react';
+import { Image } from 'semantic-ui-react';
 
 export default class HeroHeader extends React.Component { // eslint-disable-line react/prefer-stateless-function
   constructor(props) {
@@ -21,47 +21,48 @@ export default class HeroHeader extends React.Component { // eslint-disable-line
     this.state = {
       isLoading: true,
       imageUrl: null,
+      title: null,
+      subTitle: null,
+      buttonText: null,
     };
   }
 
   componentDidMount() {
     const client = contentful.createClient({ space: SPACE_ID, accessToken: ACCESS_TOKEN });
 
-    // client.getEntries()
-    //   .then(response => this.setState({ isLoading: false, navigationMenuItems: response.items }))
-    //   .catch(console.error);
-    //
+    client.getEntry(ENTRY_ID)
+      .catch(console.error)
+      .then((entry) => {
+        const { title, subTitle, buttonText } = entry.fields;
+
+        this.setState({ isLoading: false, title, subTitle, buttonText });
+      });
+
     client.getAsset(IMAGE_ID)
       .catch(console.error)
-      .then(asset => this.setState({
-        isLoading: false,
-        imageUrl: `https:${asset.fields.file.url}`,
-      }));
+      .then((asset) => {
+        const { url } = asset.fields.file;
+
+        this.setState({
+          isLoading: false,
+          imageUrl: `https:${url}`,
+        });
+      });
   }
 
   render() {
-    const { isLoading, imageUrl } = this.state
+    const { isLoading, imageUrl, title, subTitle, buttonText } = this.state
 
     return (
       <div className="top">
         <Loading isLoading={isLoading} />
 
-        <div className="slider-img-parent">
-          <img
-            alt="presentation"
-            role="presentation"
-            src={imageUrl}
-            width="auto"
-            height="500px"
-          />
-        </div>
+        <Image src={imageUrl} fluid />
 
         <div className="text-block">
-          <div className="product-title">Amazing Product Name</div>
-          <div className="product-subtitle">Amazing Product Name Description</div>
-          <p><a className="product-anchor">
-            A Link to the Amazing Product
-          </a></p>
+          <div className="product-title">{title}</div>
+          <div className="product-subtitle">{subTitle}</div>
+          <p><a className="product-anchor">{buttonText}</a></p>
         </div>
 
         <style jsx>{`
@@ -69,20 +70,6 @@ export default class HeroHeader extends React.Component { // eslint-disable-line
             position: relative;
             top: -13px;
 
-          }
-          .slider-img-parent {
-            margin: 0 auto;
-            width: 100vw;
-            top: -10px;
-            background-color:#e3e8ee;
-          }
-          .slider-img-parent img {
-            width: 100vw;
-            position: relative;
-            left: calc(-50vw + 50%);
-            display: block;
-            width: 100vw;
-            object-fit: cover;
           }
           .text-block{
             position: relative;
