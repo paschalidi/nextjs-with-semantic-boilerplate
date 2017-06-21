89ac985bb0600fa14bf21ac180a16ec3daada5c8
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-21 09:47:58 +0200

    implements reusable Loading component

---
+++ client/components/Loading/index.js
@@ -0,0 +1,30 @@
+/**
+ *
+ * Loading
+ *
+ */
+/* eslint-disable import/first */
+/* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
+
+import React from 'react';
+import { Loader, Dimmer } from 'semantic-ui-react';
+
+class Loading extends React.Component { // eslint-disable-line react/prefer-stateless-function
+
+  render() {
+    console.log('this.props')
+    console.log(this.props)
+
+    if (!this.props.isLoading) {
+      return <div></div>;
+    }
+    return (
+      <Dimmer inverted active>
+        <Loader />
+      </Dimmer>
+    );
+  }
+}
+
+export default Loading;
--- client/components/Loading/tests/index.test.js
+++ client/components/Loading/tests/index.test.js
@@ -1,9 +1,9 @@
 // import React from 'react';
 // import { shallow } from 'enzyme';
 
-// import Loader from '../index';
+// import Loading from '../index';
 
-describe('<Loader />', () => {
+describe('<Loading />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
   });
