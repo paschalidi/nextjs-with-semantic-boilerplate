68d4507af2f930e59def718e5681edceceeda1de
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-21 13:31:01 +0200

    makes footer component

--- client/containers/Footer/constants.js
+++
@@ -1,8 +0,0 @@
-/*
- *
- * Footer constants
- *
- */
-
-export const DEFAULT_ACTION = 'Footer/DEFAULT_ACTION';
-export const FETCH_CONTENTFUL = 'Footer/FETCH_CONTENTFUL';
--- client/containers/Footer/tests/reducer.test.js
+++
@@ -1,9 +0,0 @@
-
-import { fromJS } from 'immutable';
-import footerReducer from '../reducer';
-
-describe('footerReducer', () => {
-  it('returns the initial state', () => {
-    expect(footerReducer(undefined, {})).toEqual(fromJS({}));
-  });
-});
