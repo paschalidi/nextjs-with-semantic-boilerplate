4ad91330a945379c9b188697e21e4d0f34565c6c
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-23 13:56:03 +0200

    fixes small bug with next.config and global styles

--- client/components/Layout/index.js
+++ client/components/Layout/index.js
@@ -10,6 +10,9 @@
 import React from 'react';
 import Head from 'next/head';
 
+import globalStyle from '../../styles/index.css';
+import semantic from '../../semantic/dist/semantic.min.css';
+
 import GoogleTagManager from '../GoogleTagManager';
 import VimcarFooter from '../Footer';
 import NavigationMenu from '../NavigationMenu';
@@ -22,75 +25,18 @@ class Layout extends React.Component { // eslint-disable-line react/prefer-state
     return (
       <div>
         <Head>
-          <link
-            rel="stylesheet"
-            href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
-          />
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400"
-            rel="stylesheet"
-          />
+          <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"/>
 
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-          />
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-          />
-
-          <style jsx global>
-            {`
-            * {
-            -moz-box-sizing: border-box;
-            -webkit-box-sizing: border-box;
-            margin: 0;
-            padding: 0
-            }
-            body {
-            font-family: 'Open Sans', sans-serif;
-            background-color: #fbfcfc;
-            }
-            .top-10{
-            position: relative;
-            top: -7em;
-            }
-            .text-container {
-            margin: 0 auto;
-            position: relative;
-            }
-            .padding-zero {
-              padding: 0 !important ;
-            }
-            .product-title{
-            font-size: 20px;
-            }
-            .product-subtitle{
-            font-size: 15px;
-            }
-            .color-prime{
-            color: #313541;
-            }
-            .color-sec{
-            color: #777f8e;
-            }
-            .color-a{
-            color: #1f9ac1;
-            }
-            .background-color-prime{
-            background-color: #e3e8ee !important;
-            }
-            .background-color-sec{
-            background-color: #f2f2f2 !important;
-            }
-            `}
-          </style>
-          <title>{ title }</title>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <link
+              rel="stylesheet"
+              type="text/css"
+              href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+            />
+            <style dangerouslySetInnerHTML={{ __html: globalStyle }} />
+            <style dangerouslySetInnerHTML={{ __html: semantic }} />
+            <title>{ title }</title>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         </Head>
         <GoogleTagManager gtmId="GTM-5N7BT99" />
         <PageLoader />
@@ -100,7 +46,7 @@ class Layout extends React.Component { // eslint-disable-line react/prefer-state
 
         <VimcarFooter />
       </div>
-    );
+  );
   }
-}
-export default Layout;
+  }
+  export default Layout;
