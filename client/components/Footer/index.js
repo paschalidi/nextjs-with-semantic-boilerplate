2c88e09afec2faf9271cc41ad584a3b37b1bae92
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-06-29 16:31:14 +0200

    fixes code style of Footer

--- client/components/Footer/index.js
+++ client/components/Footer/index.js
@@ -15,89 +15,89 @@ export default class Footer extends React.Component { // eslint-disable-line rea
     return (
       <div className="footer">
 
-          <Grid container stackable columns={5}>
-            <Grid.Row className="pad-top-s" only="tablet computer">
-              <Grid.Column>
-                <List inverted verticalAlign="middle">
-                  <List.Item>
-                    <List.Content>
-                      <List.Header>Header</List.Header>
-                    </List.Content>
-                  </List.Item>
-                </List>
-                <List link inverted verticalAlign="middle">
-                  <List.Item>
-                    <List.Content>
-                      <List.Description>
-                        <a>Home</a>
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
-                  <List.Item>
-                    <List.Content>
-                      <List.Description>
-                        <a>Fleet Management</a>
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
-                  <List.Item>
-                    <List.Content>
-                      <List.Description>
-                        <a>Magazine</a>
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
-                  <List.Item>
-                    <List.Content>
-                      <List.Description>
-                        <a>Support</a>
-                      </List.Description>
-                    </List.Content>
-                  </List.Item>
-                </List>
-              </Grid.Column>
+        <Grid container stackable columns={5}>
+          <Grid.Row className="pad-top-s" only="tablet computer">
+            <Grid.Column>
+              <List inverted verticalAlign="middle">
+                <List.Item>
+                  <List.Content>
+                    <List.Header>Header</List.Header>
+                  </List.Content>
+                </List.Item>
+              </List>
+              <List link inverted verticalAlign="middle">
+                <List.Item>
+                  <List.Content>
+                    <List.Description>
+                      <a>Home</a>
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Description>
+                      <a>Fleet Management</a>
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Description>
+                      <a>Magazine</a>
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+                <List.Item>
+                  <List.Content>
+                    <List.Description>
+                      <a>Support</a>
+                    </List.Description>
+                  </List.Content>
+                </List.Item>
+              </List>
+            </Grid.Column>
 
-              <Grid.Column width={4} floated="right">
-                <List floated="right" className="nowrap" link inverted horizontal>
-                  <List.Item><Icon name="phone" /></List.Item>
-                  <List.Item>+49 1639 68 30 10</List.Item>
-                </List>
+            <Grid.Column width={4} floated="right">
+              <List floated="right" className="nowrap" link inverted horizontal>
+                <List.Item><Icon name="phone" /></List.Item>
+                <List.Item>+49 1639 68 30 10</List.Item>
+              </List>
 
-                <List floated="right" className="nowrap" link inverted horizontal>
-                  <List.Item><Icon name="mail outline" /></List.Item>
-                  <List.Item>kontakt@vimcar.com</List.Item>
-                </List>
-                <br />
-                <br />
-                <br />
-                <br />
-                <br />
-                <List floated="right" className="padding-top nowrap" link inverted horizontal>
-                  <List.Item as="a"><Icon name="facebook f" /></List.Item>
-                  <List.Item as="a"><Icon name="twitter" /></List.Item>
-                  <List.Item as="a"><Icon name="google plus" /></List.Item>
-                </List>
-              </Grid.Column>
-            </Grid.Row>
+              <List floated="right" className="nowrap" link inverted horizontal>
+                <List.Item><Icon name="mail outline" /></List.Item>
+                <List.Item>kontakt@vimcar.com</List.Item>
+              </List>
+              <br />
+              <br />
+              <br />
+              <br />
+              <br />
+              <List floated="right" className="padding-top nowrap" link inverted horizontal>
+                <List.Item as="a"><Icon name="facebook f" /></List.Item>
+                <List.Item as="a"><Icon name="twitter" /></List.Item>
+                <List.Item as="a"><Icon name="google plus" /></List.Item>
+              </List>
+            </Grid.Column>
+          </Grid.Row>
 
-            <Grid.Row className="pad-top-s" only="computer tablet">
-              <Grid.Column width={8}>
-                <List link divided inverted horizontal>
-                  <List.Item><a>About us</a></List.Item>
-                  <List.Item><a>Contact</a></List.Item>
-                  <List.Item><a>Support</a></List.Item>
-                </List>
-              </Grid.Column>
+          <Grid.Row className="pad-top-s" only="computer tablet">
+            <Grid.Column width={8}>
+              <List link divided inverted horizontal>
+                <List.Item><a>About us</a></List.Item>
+                <List.Item><a>Contact</a></List.Item>
+                <List.Item><a>Support</a></List.Item>
+              </List>
+            </Grid.Column>
 
-              <Grid.Column floated="right">
-                <List link floated="right" divided inverted horizontal>
-                  <List.Item><a>EN</a></List.Item>
-                  <List.Item><a>DE</a></List.Item>
-                  <List.Item><a>SP</a></List.Item>
-                </List>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
+            <Grid.Column floated="right">
+              <List link floated="right" divided inverted horizontal>
+                <List.Item><a>EN</a></List.Item>
+                <List.Item><a>DE</a></List.Item>
+                <List.Item><a>SP</a></List.Item>
+              </List>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
 
         <Grid divided inverted stackable columns={4}>
           <Grid.Row
@@ -129,7 +129,8 @@ export default class Footer extends React.Component { // eslint-disable-line rea
                 </List.Item>
                 <List.Item className="sm-pad-s">
                   <List.Content>
-                    <List.Header className="list-header-pad-s"> <Icon name="mail outline" /> </List.Header>
+                    <List.Header className="list-header-pad-s"> <Icon name="mail outline" />
+                    </List.Header>
                     <List.Description><b>kontakt@vimcar.de</b></List.Description>
                   </List.Content>
                 </List.Item>
