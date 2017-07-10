7dd1d084f5bf79d16be49ea1da81354f71aa837e
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2017-07-10 09:55:21 +0200

    completes Single refactoring and deletes HeroHeader

--- client/components/Single/index.js
+++ client/components/Single/index.js
@@ -16,13 +16,17 @@ class Single extends React.Component { // eslint-disable-line react/prefer-state
 
   static defaultProps = {
     hasImageOnRight: false,
+    hasDoubleImage: false,
     hasNoContainer: false,
     hasColorPrime: false,
   };
 
   render() {
-    const { hasImageOnRight, hasNoContainer, hasColorPrime } = this.props;
-    const { image, title, subtitle, anchor } = this.props.contentfulData;
+    const { hasImageOnRight, hasNoContainer, hasColorPrime, hasDoubleImage } = this.props;
+    const { image, imageLeft, imageRight, title, subtitle, anchor } = this.props.contentfulData;
+
+    console.log(imageLeft)
+    console.log(hasDoubleImage)
 
     if (hasImageOnRight) {
       return (
@@ -54,6 +58,43 @@ class Single extends React.Component { // eslint-disable-line react/prefer-state
               <div className={hasColorPrime ? "background-color-prime" : "background-color-sec"}>
                 <Image src={image} fluid />
               </div>
+            </Grid.Column>
+          </Grid>
+        </div>
+      )
+    }
+    else if (hasDoubleImage) {
+      return (
+        <div>
+          <Grid className="header-full-width-s" centered stretched stackable textAlign="left">
+            <Grid.Column
+              className="left-no-padding-container bottom-s text-container"
+              mobile={16}
+              tablet={8}
+              computer={8}
+            >
+
+              <div className="container-overlay background-color-prime"
+                   style={{ backgroundImage: `url(${imageLeft})` }}>
+                <div className="text-block text-pad-left">
+                  <div className="floating-text-content">
+                    <h1>{title}</h1>
+                    <div className="color-sec">{subtitle}</div>
+                    {anchor ? <Button>{anchor}</Button> : <span></span>}
+                  </div>
+                </div>
+              </div>
+
+            </Grid.Column>
+
+
+            <Grid.Column
+              className="right-no-padding-container top-s"
+              mobile={16}
+              tablet={8}
+              computer={8}
+            >
+              <Image className="background-color-prime" src={imageRight} fluid />
             </Grid.Column>
           </Grid>
         </div>
